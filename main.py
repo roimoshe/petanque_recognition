960558ac8a070700e4c096826f6a47b724aaa3a3
Roi Moshe <rmoshe@apple.com>
2021-02-13 17:22:25 +0200

    add option for quick run(without saving the image after every step)

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def main(arguments):
     parser = argparse.ArgumentParser(description="Petanque recognition")
     parser.add_argument("-s","--step", type=int, help="Input step number to start from", default=1)
     parser.add_argument("-i","--image_num", type=int, help="Input image number", default=1)
+    parser.add_argument("-q","--quick", action='store_true', help="Input image number")
     args = parser.parse_args(arguments)
 
     if args.image_num < 0 or args.step > 17:
@@ -28,15 +29,20 @@ def main(arguments):
         print("step: ", args.step," not supported")
         return 2
     elif args.step  == 1:
+        print("image_num: ", args.image_num,", start step: ", args.step)
         img_path = 'images/image{}.jpg'.format(args.image_num)
         img = cv2.imread(img_path)
-        util.save_photo('build/image{}.jpg'.format(args.image_num),img, True)
+        if not args.quick:
+            util.save_photo('build/image{}.jpg'.format(args.image_num),img, True)
     else:
+        print("image_num: ", args.image_num,", start step: ", args.step)
         img = cv2.imread('build/image{}_step{}.jpg'.format(args.image_num, args.step - 1))
-    print("image_num: ", args.image_num,", start step: ", args.step)
 
     for i in range(args.step - 1, len(steps)):
         img = steps[i](img)
+        if not args.quick:
+            util.save_photo('build/image{}_step{}.jpg'.format(args.image_num, i+1),img, True)
+    if args.quick:
         util.save_photo('build/image{}_step{}.jpg'.format(args.image_num, i+1),img, True)
     return 0
 
